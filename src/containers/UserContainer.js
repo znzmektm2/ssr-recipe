90c5bf55caf162647a05fbeaa5b8cbe079379f10
znzmektm2 <tingcber2@naver.com>
2020-06-03 09:21:31 +0900

    오타수정

--- src/containers/UserContainer.js
+++ src/containers/UserContainer.js
@@ -9,7 +9,7 @@ const UserContainer = ({ id }) => {
   const user = useSelector(state => state.users.user);
   const dispatch = useDispatch();
 
-  usePreloader(() => dispatch(getUser(id))); // 서버 사이드 렌더링을 할 때 APU 호출하기
+  usePreloader(() => dispatch(getUser(id))); // 서버 사이드 렌더링을 할 때 API 호출하기
   useEffect(() => {
     if (user && user.id === parseInt(id, 10)) return; // 사용자가 존재하고, id가 일치한다면 요청하지 않음
     dispatch(getUser(id));
